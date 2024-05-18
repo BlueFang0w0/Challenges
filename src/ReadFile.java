22cc8f38a2b8efd662ffa2d726bbd321624f5b0f
BlueFang0w0 <lukjanovs1999@gmail.coom>
2024-05-18 03:11:00 +0100

    v2

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ReadFile {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ReadFile {
+    public static void processFile(){
+        // Task consists of three parts.
+        // 1. File location.
+        // 2. Test for file.
+        // 3. Read the file and output its contents.
+
+        File textFile = new File("src/files/Sample Text.txt"); // File location.
+
+        if(textFile.exists()){ //Testing for file.
+            System.out.println("File Located.");
+        }else{
+            System.out.println("Absolute Path: " + textFile.getAbsolutePath());
+            System.out.println("Parent Directory: " + textFile.getParent());
+        }
+
+        try{
+            String line;
+            Scanner stdin = new Scanner(textFile);
+            while(stdin.hasNextLine()){ // Output the file contents.
+                line = stdin.nextLine();
+                System.out.println(line);
+            }
+        }
+        catch (
+                FileNotFoundException a){
+        }
+    }
+
 }
